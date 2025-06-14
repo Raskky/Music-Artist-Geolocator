a28ffd9ca8fe68091af9833064ddfd100ff3d53e
Raskky <alexandergeraskov1@gmail.com>
2025-06-14 17:09:22 +0300

    add better logic to search for area's MBID

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import MaplibreGeocoder from "@maplibre/maplibre-gl-geocoder";
 import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 
+const origin = document.getElementById("origin");
+const artistList = document.getElementById("artists");
+const searchOptions = document.getElementById("select-search-options");
+
 const mbApi = new MusicBrainzApi({
   appName: "artist-map",
   appVersion: "0.0.1",
@@ -13,7 +17,7 @@ const mbApi = new MusicBrainzApi({
 const map = new maplibregl.Map({
   container: "map", // container id
   style: "https://tiles.openfreemap.org/styles/dark", // style URL
-  center: [-74, 41], // starting position [lng, lat]
+  center: [-123.4, 47.9], // starting position [lng, lat]
   zoom: 6, // starting zoom
 });
 
@@ -21,7 +25,6 @@ const geocoderApi = {
   forwardGeocode: async (config) => {
     let location = "";
     const features = [];
-    const searchOptions = document.getElementById("select-search-options");
     if (searchOptions.value === "artist") {
       try {
         const mbApiResult = await mbApi.search("artist", {
@@ -36,7 +39,6 @@ const geocoderApi = {
         if (typeof mbApiResultCountry !== "undefined") {
           location = `${mbApiResultCity}, ${mbApiResultCountry}`;
         } else location = mbApiResultCity;
-        const origin = document.getElementById("origin");
         origin.setAttribute("style", "display: block;");
         origin.innerHTML = location;
       } catch (error) {
@@ -106,7 +108,7 @@ geocoderContainer.appendChild(selectSearchOptions);
 geocoder.on("result", (e) => {
   clearResult();
   //console.log(e.result);
-  searchAndDisplayArtists(e.result.city, e.result.country_code);
+  //searchAndDisplayArtists(e.result.city, e.result.country_code);
 });
 
 map.on("mousemove", (e) => {
@@ -120,81 +122,148 @@ map.on("mousemove", (e) => {
 
 map.on("click", async (e) => {
   clearResult();
-  try {
-    const response = await fetch(
-      `https://nominatim.openstreetmap.org/reverse?lat=${e.lngLat.lat}&lon=${e.lngLat.lng}&format=geojson&polygon_geojson=1&addressdetails=1`,
-    );
-    const data = await response.json();
-    if (data.features) {
-      searchAndDisplayArtists(data.features[0]);
-    }
-  } catch (error) {
-    console.error("Error reverse geocoding coordinates", error);
-    return [];
+  const location = await getLocationFromCoords(e.lngLat.lat, e.lngLat.lng);
+  console.log(location);
+  let areaMBID = await getAreaMBID(
+    location.city,
+    location.countryCode,
+    location.region,
+  );
+  if (!areaMBID) {
+    areaMBID = await getAreaMBID(location.city, location.countryCode);
+  }
+  if (!areaMBID) {
+    areaMBID = await getAreaMBID(location.city);
+  }
+  console.log(areaMBID);
+  const artists = await getArtistsFromArea(areaMBID);
+  const randomArtists = await getRandomArtists(artists, 10);
+
+  if (randomArtists && randomArtists.length > 0) {
+    origin.innerHTML = `${location.city}, ${location.countryCode}`;
+    origin.setAttribute("style", "display: block;");
+    const p = document.createElement("p");
+    artistList.setAttribute("style", "display: block;");
+    p.innerHTML = `<b>10 Artists from ${location.city}, ${location.countryCode}</b>`;
+    artistList.appendChild(p);
+
+    randomArtists.forEach((a) => {
+      const ul = document.createElement("ul");
+      ul.innerHTML = a.name;
+      artistList.appendChild(ul);
+    });
+  } else {
+    clearResult();
   }
 });
 
-async function searchAndDisplayArtists(feature) {
-  console.log(feature);
-  const countryCode = feature.properties.address["country_code"].toUpperCase();
-  let city = "";
-  if (feature.properties.address.city) {
-    city = feature.properties.address.city;
-  } else city = feature.properties.address.town;
-  const origin = document.getElementById("origin");
-  const artistList = document.getElementById("artists");
-  let areaOnClick = [];
+async function getLocationFromCoords(lat, lon) {
   try {
-    const area_response = await mbApi.search("area", {
-      query: `iso:"${feature.properties.address["ISO3166-2-lvl4"]}" AND area:"${city}"`,
-    });
-    areaOnClick = area_response.areas[0];
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`,
+    );
+    const data = await response.json();
+    return {
+      city: data.address.city || data.address.town,
+      region: data.address.state || data.address.region,
+      country: data.address.country,
+      countryCode: data.address.country_code?.toUpperCase(), // e.g., "US"
+    };
   } catch (error) {
-    console.error("Error fetching area: ", error);
+    console.log("Error reverse geocoding area: ", error);
     return [];
   }
+}
 
+async function getAreaMBID(locationName, countryCode, regionName) {
   try {
-    const artist_response = await mbApi.browse("artist", {
-      area: areaOnClick.id,
+    let query = `"${locationName}"`;
+    if (regionName) query += `AND "${regionName}"`;
+    if (countryCode) query += `AND "${countryCode}"`;
+
+    const response = await fetch(
+      `https://musicbrainz.org/ws/2/area/?query=${encodeURIComponent(query)}&fmt=json`,
+    );
+    const data = await response.json();
+
+    const matchedArea = data.areas?.find(
+      (area) =>
+        area?.["iso-3166-2-codes"]?.includes(countryCode) ||
+        area.name.toLowerCase() === locationName.toLowerCase(),
+    );
+    return matchedArea?.id;
+  } catch (error) {
+    console.error("Error getting area's MBID: ", error);
+    return null;
+  }
+}
+
+async function getArtistsFromArea(areaMBID) {
+  try {
+    const response = await mbApi.browse("artist", {
+      area: areaMBID,
       limit: 100,
     });
-    const artists = artist_response.artists;
-    //console.log(artist_response);
-    const artistNames = artists.map((artist) => artist.name);
-    const n = 10;
-    const randomArtists = getRandomArtists(artistNames, n);
-
-    if (randomArtists && randomArtists.length > 0) {
-      origin.innerHTML = `${city}, ${countryCode}`;
-      origin.setAttribute("style", "display: block;");
-      const p = document.createElement("p");
-      artistList.setAttribute("style", "display: block;");
-      p.innerHTML = `<b>${n} Artists from ${city}, ${countryCode}</b>`;
-      artistList.appendChild(p);
-
-      randomArtists.forEach((a) => {
-        const ul = document.createElement("ul");
-        ul.innerHTML = a;
-        artistList.appendChild(ul);
-      });
-    } else {
-      clearResult();
-    }
+    const artists = response.artists;
+    return artists;
   } catch (error) {
-    console.error("Error fetching artists: ", error);
+    console.error("Error browsing artists from area: ", error);
     return [];
   }
 }
 
-function clearResult() {
-  const origin = document.getElementById("origin");
-  const artistList = document.getElementById("artists");
-  origin.innerHTML = "";
-  origin.setAttribute("style", "display: none;");
-  artistList.innerHTML = "";
-  artistList.setAttribute("style", "display: none;");
-}
+// sync function searchAndDisplayArtists(feature) {
+//   console.log(feature);
+//   const countryCode = feature.properties.address["country_code"].toUpperCase();
+//   let city = "";
+//   if (feature.properties.address.city) {
+//     city = feature.properties.address.city;
+//   } else city = feature.properties.address.town;
+//   const origin = document.getElementById("origin");
+//   const artistList = document.getElementById("artists");
+//   let areaOnClick = [];
+//   try {
+//     const area_response = await mbApi.search("area", {
+//       query: `iso:"${feature.properties.address["ISO3166-2-lvl4"]}" AND area:"${city}"`,
+//     });
+//     areaOnClick = area_response.areas[0];
+//   } catch (error) {
+//     console.error("Error fetching area: ", error);
+//     return [];
+//   }
+
+//   try {
+//     const artist_response = await mbApi.browse("artist", {
+//       area: areaOnClick.id,
+//       limit: 100,
+//     });
+//     const artists = artist_response.artists;
+//     //console.log(artist_response);
+//     const artistNames = artists.map((artist) => artist.name);
+//     const n = 10;
+//     const randomArtists = getRandomArtists(artistNames, n);
+
+//     if (randomArtists && randomArtists.length > 0) {
+//       origin.innerHTML = `${city}, ${countryCode}`;
+//       origin.setAttribute("style", "display: block;");
+//       const p = document.createElement("p");
+//       artistList.setAttribute("style", "display: block;");
+//       p.innerHTML = `<b>${n} Artists from ${city}, ${countryCode}</b>`;
+//       artistList.appendChild(p);
+
+//       randomArtists.forEach((a) => {
+//         const ul = document.createElement("ul");
+//         ul.innerHTML = a;
+//         artistList.appendChild(ul);
+//       });
+//     } else {
+//       clearResult();
+//     }
+//   } catch (error) {
+//     console.error("Error fetching artists: ", error);
+//     return [];
+//   }
+// }
 
 function getRandomArtists(artists, n) {
   try {
@@ -205,4 +274,11 @@ function getRandomArtists(artists, n) {
   } catch (error) {
     console.error("Couldn't get random artists", error);
   }
+}
+
+function clearResult() {
+  origin.innerHTML = "";
+  origin.setAttribute("style", "display: none;");
+  artistList.innerHTML = "";
+  artistList.setAttribute("style", "display: none;");
 }
