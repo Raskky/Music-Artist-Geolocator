7f4b51f4a6fe06db3f7c264d9d3aefc144d6906a
Raskky <alexandergeraskov1@gmail.com>
2025-06-14 22:06:54 +0300

    remove irrelevant code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import maplibregl from "maplibre-gl";
 import { MusicBrainzApi } from "musicbrainz-api";
-import MaplibreGeocoder from "@maplibre/maplibre-gl-geocoder";
 import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 const origin = document.getElementById("origin");
 const artistList = document.getElementById("artists");
-const searchOptions = document.getElementById("select-search-options");
+const styles = {
+  dark: "https://tiles.openfreemap.org/styles/dark",
+};
 
 const mbApi = new MusicBrainzApi({
   appName: "artist-map",
@@ -19,105 +20,6 @@ const map = new maplibregl.Map({
   style: "https://tiles.openfreemap.org/styles/dark", // style URL
   center: [-123.4, 47.9], // starting position [lng, lat]
   zoom: 6, // starting zoom
-});
-
-// const geocoderApi = {
-//   forwardGeocode: async (config) => {
-//     let location = "";
-//     const features = [];
-//     if (searchOptions.value === "artist") {
-//       try {
-//         const mbApiResult = await mbApi.search("artist", {
-//           query: `${config.query}`,
-//         });
-//         const mbApiResultCity = mbApiResult.artists.find(
-//           (artist) => artist.name.toLowerCase() == config.query.toLowerCase(),
-//         )["begin-area"]["name"];
-//         const mbApiResultCountry = mbApiResult.artists.find(
-//           (artist) => artist.name.toLowerCase() == config.query.toLowerCase(),
-//         )["country"];
-//         if (typeof mbApiResultCountry !== "undefined") {
-//           location = `${mbApiResultCity}, ${mbApiResultCountry}`;
-//         } else location = mbApiResultCity;
-//         origin.setAttribute("style", "display: block;");
-//         origin.innerHTML = location;
-//       } catch (error) {
-//         console.error("Error parsing artist's properties\n", error);
-//       }
-//     }
-//     if (searchOptions.value === "area") {
-//       location = config.query;
-//       try {
-//         const request = `https://nominatim.openstreetmap.org/search?q=${
-//           location
-//         }&format=geojson&polygon_geojson=1&addressdetails=1`;
-//         const response = await fetch(request);
-//         const geojson = await response.json();
-//         for (const feature of geojson.features) {
-//           const center = [
-//             feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
-//             feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
-//           ];
-//           const point = {
-//             type: "Feature",
-//             geometry: {
-//               type: "Point",
-//               coordinates: center,
-//             },
-//             place_name: feature.properties.display_name,
-//             country_code: feature.properties.address.country_code,
-//             city: feature.properties.address.city,
-//             properties: feature.properties,
-//             text: feature.properties.display_name,
-//             place_type: ["place"],
-//             center,
-//           };
-//           features.push(point);
-//         }
-//         //console.log(features);
-//       } catch (e) {
-//         console.error(`Failed to forwardGeocode with error: ${e}`);
-//       }
-//       return {
-//         features,
-//       };
-//     }
-//   },
-// };
-
-// const geocoder = new MaplibreGeocoder(geocoderApi, {
-//   //showResultsWhileTyping: true,
-//   minLength: 3,
-//   maplibregl,
-// });
-
-// const geocoderContainer = document.getElementById("geocoder-container");
-// geocoderContainer.appendChild(geocoder.onAdd(map));
-// const artistOption = document.createElement("option");
-// artistOption.id = "artist-option";
-// artistOption.innerHTML = "artist";
-// const areaOption = document.createElement("option");
-// areaOption.id = "area-option";
-// areaOption.innerHTML = "area";
-// const selectSearchOptions = document.createElement("select");
-// selectSearchOptions.id = "select-search-options";
-// selectSearchOptions.options.add(artistOption);
-// selectSearchOptions.options.add(areaOption);
-// geocoderContainer.appendChild(selectSearchOptions);
-
-// geocoder.on("result", (e) => {
-//   clearResult();
-//   //console.log(e.result);
-//   //searchAndDisplayArtists(e.result.city, e.result.country_code);
-// });
-
-map.on("mousemove", (e) => {
-  try {
-    document.getElementById("info").innerHTML =
-      `${JSON.stringify(e.point)}<br />${JSON.stringify(e.lngLat.wrap())}`;
-  } catch (error) {
-    console.error("Error getting coordinates under mouse cursor: ", error);
-  }
 });
 
 map.on("click", async (e) => {
